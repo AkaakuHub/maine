58808bf5a30ec0749469491cb6ad9c842c89e48a
AkaakuHub <akaaku2021media01@gmail.com>
2025-06-06 15:19:01 +0900

    fix: del unused import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,6 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-import { rules } from "eslint-plugin-react";
 
 export default defineConfig([
     {
